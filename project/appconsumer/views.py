dd5927719b9aef5fcf8342cf02be48489aaeec89
Jhonatas Kleinkauff <jhonatas.kleinkauff@loadsmart.com>
2021-02-15 13:52:11 -0300

    add volume

--- project/appconsumer/views.py
+++ project/appconsumer/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import redis
+import requests
 
 r = redis.Redis(host="redis-server", db=0)
+# r = redis.Redis(host="localhost", db=0)
 r.set("visits", 0)
 
 def index(request):
+    request = requests.get('https://api.github.com/user', auth=('user', 'pass'))
     v = int(r.get("visits"))
     ret = f"Hello, world. You're at the polls index. Number of visits:{v}"
     r.set("visits", v+1)
